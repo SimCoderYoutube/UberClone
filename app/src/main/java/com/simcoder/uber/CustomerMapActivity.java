1ad0b3121575f690d15076fa0cf1c0b1cc41f596
simpleCoder <simplecodervideos@gmail.com>
2017-10-11 12:03:11 +0100

    - Ride History RecyclerView getInformation form database

--- app/src/main/java/com/simcoder/uber/CustomerMapActivity.java
+++ app/src/main/java/com/simcoder/uber/CustomerMapActivity.java
@@ -182,6 +182,7 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CustomerMapActivity.this, HistoryActivity.class);
+                intent.putExtra("customerOrDriver", "Customers");
                 startActivity(intent);
                 return;
             }
--- app/src/main/java/com/simcoder/uber/HistoryActivity.java
+++ app/src/main/java/com/simcoder/uber/HistoryActivity.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.simcoder.uber.historyRecyclerView.HistoryAdapter;
 import com.simcoder.uber.historyRecyclerView.HistoryObject;
 
@@ -12,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryActivity extends AppCompatActivity {
+    private String customerOrDriver, userId;
 
     private RecyclerView mHistoryRecyclerView;
     private RecyclerView.Adapter mHistoryAdapter;
@@ -30,12 +37,45 @@ public class HistoryActivity extends AppCompatActivity {
         mHistoryAdapter = new HistoryAdapter(getDataSetHistory(), HistoryActivity.this);
         mHistoryRecyclerView.setAdapter(mHistoryAdapter);
 
-        for (int i=0;i<100;i++){
-            HistoryObject obj = new HistoryObject(Integer.toString(i));
-            resultsHistory.add(obj);
-        }
 
-        mHistoryAdapter.notifyDataSetChanged();
+        customerOrDriver = getIntent().getExtras().getString("customerOrDriver");
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        getUserHistoryIds();
+    }
+
+    private void getUserHistoryIds() {
+        DatabaseReference userHistoryDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(customerOrDriver).child(userId).child("history");
+        userHistoryDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot history : dataSnapshot.getChildren()){
+                        FetchRideInformation(history.getKey());
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void FetchRideInformation(String rideKey) {
+        DatabaseReference historyDatabase = FirebaseDatabase.getInstance().getReference().child("history").child(rideKey);
+        historyDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    String rideId = dataSnapshot.getKey();
+                    HistoryObject obj = new HistoryObject(rideId);
+                    resultsHistory.add(obj);
+                    mHistoryAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 
     private ArrayList resultsHistory = new ArrayList<HistoryObject>();
