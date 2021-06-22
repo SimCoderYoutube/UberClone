4f930b9182af7dfe36e7e90df3d13acae3c59449
8uu <abutorky.100@gmail.com>
2021-06-22 22:00:29 +0300

    paypal didnt work
    C:\Users\Saad.PC-RYV1\StudioProjects\UberClone3\android\app\src\main\java\com\binsaleh\hover\HistorySingleActivity.java:199: error: cannot find symbol
                .clientId(PayPalConfig.PAYPAL_CLIENT_ID);
                          ^
      symbol:   variable PayPalConfig
      location: class HistorySingleActivity

---
+++ android/app/src/main/java/com/binsaleh/hover/historyRecyclerView/HistoryObject.java
@@ -0,0 +1,25 @@
+package com.binsaleh.hover.historyRecyclerView;
+
+/**
+ * Created by manel on 10/10/2017.
+ */
+
+public class HistoryObject {
+    private String rideId;
+    private String time;
+
+    public HistoryObject(String rideId, String time){
+        this.rideId = rideId;
+        this.time = time;
+    }
+
+    public String getRideId(){return rideId;}
+    public void setRideId(String rideId) {
+        this.rideId = rideId;
+    }
+
+    public String getTime(){return time;}
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
